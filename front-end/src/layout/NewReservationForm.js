e30d2aea2b6d9f7549a60d46dd94bc0a253e68e7
Basil Martin <imconnormartin@gmail.com>
2022-09-19 14:19:15 -0400

    Edit&Create Res now use same component

--- front-end/src/layout/NewReservationForm.js
+++ front-end/src/layout/NewReservationForm.js
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
-import ErrorAlert from "./ErrorAlert";
+import ReservationForm from "./ReservationForm";
+
 
 const { REACT_APP_API_BASE_URL } = process.env;
 
 function NewReservations() {
+
+  const history = useHistory();
+
   const initialFormState = {
     first_name: "",
     last_name: "",
@@ -15,16 +19,26 @@ function NewReservations() {
   };
 
   const [formData, setFormData] = useState({ ...initialFormState });
-  const [error, setError] = useState(undefined);
-  const [errorMessage, setErrorMessage] = useState(undefined);
+  const [error, setError] = useState(null);
+  //const [errorMessage, setErrorMessage] = useState(undefined);
 
   useEffect(() => {
     const abortController = new AbortController();
-    setErrorMessage(error);
+    setError(error);
     return () => abortController.abort();
   }, [error]);
 
-  const history = useHistory();
+  const handleInputChange = (event) => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.name === "people" ? Number(event.target.value) : event.target.value
+    });
+  };
+
+  const handleCancel = () => {
+    setFormData({ ...initialFormState });
+    history.goBack();
+  };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -50,106 +64,22 @@ function NewReservations() {
     }
   };
 
-  const handleCancel = () => {
-    setFormData({ ...initialFormState });
-    history.goBack();
-  };
 
-  const handleInputChange = (event) => {
-    setFormData({
-      ...formData,
-      [event.target.name]: event.target.name === "people" ? Number(event.target.value) : event.target.value
-    });
-  };
 
-  const formElement = (
-    <form className="form" onSubmit={handleSubmit}>
-      <label htmlFor="first_name">
-        first name:
-        <input
-          type="text"
-          name="first_name"
-          id="first_name"
-          onChange={handleInputChange}
-          value={formData.first_name}
-          required
-        />
-      </label>
 
-      <label htmlFor="last_name">
-        last name:
-        <input
-          type="text"
-          name="last_name"
-          id="last_name"
-          onChange={handleInputChange}
-          value={formData.last_name}
-          required
-        />
-      </label>
 
-      <label htmlFor="mobile_number">
-        mobile number:
-        <input
-          type="text"
-          name="mobile_number"
-          id="mobile_number"
-          placeholder="xxx-xxx-xxxx"
-          onChange={handleInputChange}
-          value={formData.mobile_number}
-          required
-        />
-      </label>
-
-      <label htmlFor="reservation_date">
-        reservation date:
-        <input
-          type="date"
-          name="reservation_date"
-          id="reservation_date"
-          onChange={handleInputChange}
-          value={formData.reservation_date}
-          required
-        />
-      </label>
-
-      <label htmlFor="reservation_time">
-        reservation time:
-        <input
-          type="time"
-          name="reservation_time"
-          id="reservation_time"
-          onChange={handleInputChange}
-          value={formData.reservation_time}
-          required
-        />
-      </label>
-
-      <label htmlFor="people">
-        amount of people:
-        <input
-          type="number"
-          name="people"
-          id="people"
-          onChange={handleInputChange}
-          value={formData.people}
-          required
-        />
-      </label>
-      <button type="button" className="btn-danger text-dark" onClick={handleCancel}>
-        
-        Cancel
-      </button>
-      <button type="submit" className="btn-primary text-dark"> Submit </button>
-    </form>
-  );
+  
 
   return (
     <div>
-      {error ? <ErrorAlert errorMessage={errorMessage}/> : <></>}
-      <div className="form-group">
-      {formElement}
-      </div>
+      <ReservationForm 
+      reservationData={formData}
+      setReservationData={setFormData}
+      submitHandler={handleSubmit}
+      cancelHandler={handleCancel}
+      error={error}
+      changeHandler={handleInputChange}
+      />
     </div>
     
 
---
+++ front-end/src/layout/ReservationForm.js
@@ -0,0 +1,105 @@
+import React from "react";
+import ErrorAlert from "./ErrorAlert";
+
+
+function ReservationForm({reservationData, setReservationData, submitHandler, cancelHandler, error, changeHandler}) {
+
+    return (
+        <div>
+          <ErrorAlert error={error} /> 
+          <form className="needs-validation center " onSubmit={submitHandler}>
+            <div className="form-row">
+              <div className="col-md-4 mb-3">
+                <label htmlFor="first_name" className="mr-2">First Name</label>
+                <input
+                  required
+                  type="text"
+                  id="first_name"
+                  name="first_name"
+                  value={reservationData.first_name}
+                  className="background_color"
+                  onChange={changeHandler}
+                ></input>
+              </div>
+              <div className="col-md-4 mb-3">
+                <label htmlFor="last_name" className="mr-2">Last Name</label>
+                <input
+                  required
+                  type="text"
+                  id="last_name"
+                  name="last_name"
+                  value={reservationData.last_name}
+                  className="background_color"
+                  onChange={changeHandler}
+                ></input>
+              </div>
+              <div className="col-md-4 mb-3">
+                <label htmlFor="mobile_number" className="mr-2">Reservation Contact Number</label>
+                <input
+                  required
+                  type="text"
+                  id="mobile_number"
+                  name="mobile_number"
+                  placeholder="xxx-xxx-xxxx"
+                  value={reservationData.mobile_number}
+                  className="background_color"
+                  onChange={changeHandler}
+                ></input>
+              </div>
+              <div className="col-md-4 mb-3">
+                <label htmlFor="reservation_date" className="mr-2">Reservation Date</label>
+                <input
+                  required
+                  type="date"
+                  id="reservation_date"
+                  name="reservation_date"
+                  placeholder={new Date()}
+                  value={reservationData.reservation_date}
+                  className="background_color"
+                  onChange={changeHandler}
+                ></input>
+              </div>
+              <div className="col-md-4 mb-3">
+                <label htmlFor="reservation_time" className="mr-2">Reservation Start Time</label>
+                <input
+                  required
+                  type="time"
+                  id="reservation_time"
+                  name="reservation_time"
+                  value={reservationData.reservation_time}
+                  onChange={changeHandler}
+                  className="background_color"
+                ></input>
+              </div>
+              <div className="col-md-4 mb-3">
+                <label htmlFor="people" className="mr-2">Number of Guests</label>
+                <input
+                  required
+                  type="number"
+                  id="people"
+                  name="people"
+                  value={reservationData.people}
+                  onChange={changeHandler}
+                  className="background_color"
+                ></input>
+              </div>
+              </div>
+              <br />
+              <button
+                type="button"
+                name="cancel-btn"
+                onClick={cancelHandler}
+                className="btn btn-primary button-position"
+              >
+                Cancel
+              </button>
+              <button type="submit" name="submit-btn" className="btn btn-secondary button-position">
+                Submit
+              </button>
+            
+          </form>
+        </div>
+      );
+}
+
+export default ReservationForm;
